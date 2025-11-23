048402fdd51093af3d2b0fe062a0785cd89211ac
nikleban <nikkatleban@gmail.com>
2025-11-23 11:34:10 +0100

    add fe var

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+
+const devUrl = "http://localhost:5000/api";
 // Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_DOMAIN_NAME || devUrl,
   withCredentials: true,
 });
 
