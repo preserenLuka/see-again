b4eb9dcb2b19d7b53867f3c7d8731aebd15c03c1
nikleban <nikkatleban@gmail.com>
2025-11-23 12:14:06 +0100

    Rename nameđ

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5000/api", "see-again.madebyluka.com",],
+    origin: ["http://localhost:5000/api", "https://see-again.madebyluka.com",],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
